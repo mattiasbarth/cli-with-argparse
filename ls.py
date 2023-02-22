ae1ca048d785bb5f0a4772109cffc4c9ceb25037
Mattias Barth <mattias.barth@gmail.com>
2023-02-22 10:06:12 +0100

    add function for flags

--- ls.py
+++ ls.py
@@ -1,10 +1,13 @@
 import argparse
+import datetime
 from pathlib import Path
 
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("path")
+
+parser.add_argument("-l", "--long", action="store_true")
 
 args = parser.parse_args()
 
@@ -14,5 +17,15 @@ if not target_dir.exists():
     print("The target directory doesn't exist")
     raise SystemExit(1)
 
+def build_output(entry, long=False):
+    if long:
+        size = entry.stat().st_size
+        date = datetime.datetime.fromtimestamp(
+            entry.stat().st_mtime).strftime(
+            "%b %d %H:%M:%S"
+        )
+        return f"{size:>6d} {date} {entry.name}"
+    return entry.name
+
 for entry in target_dir.iterdir():
-    print(entry.name)
+    print(build_output(entry, long=args.long))
