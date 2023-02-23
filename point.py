c8b78ea1373e304d3d1ff58c6fcb8721a7bed0f5
Mattias Barth <mattias.barth@gmail.com>
2023-02-23 14:48:28 +0100

    update point.py and add groups.py

--- point.py
+++ point.py
@@ -3,7 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--coordinates", nargs=2)
+parser.add_argument(
+    "--coordinates",
+    nargs=2,
+    metavar=("X", "Y"),
+    help="take the Cartesian coordinates %(metavar)s",
+    )
 
 args = parser.parse_args()
 
